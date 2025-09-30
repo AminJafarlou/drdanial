e7b2fc1c358fa79c39e0d92183d8df9f501753c0
aminjafarlou <jafarlou.amin@gmail.com>
2025-09-30 21:17:24 +0330

    Fix header for mobile

--- src/components/CoverImageSection.tsx
+++ src/components/CoverImageSection.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export const CoverImageSection = () => {
   return (
-    <section className="cover-image-section relative">
-        <Image fill src="/cover.jpg" alt="Cover" className="w-full h-auto" />
+    <section className="cover-image-section relative w-full h-[500px]">
+      <Image src="/cover.jpg" alt="Cover" fill priority className="object-cover" />
     </section>
   )
 }
